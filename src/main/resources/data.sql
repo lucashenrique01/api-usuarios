9fecaeacfd80fe443d8f2d0d5ef911bcf93982cb
lucashenrique01 <lucas.paula@bandtec.com>
2022-09-20 21:16:14 -0300

    adicionando data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+use nutrion;
+
+INSERT INTO tb_usuarios
+(nome_usuario, email_usuario, senha_usuario, autenticado, experiencia, tipo_conta, avaliacao)
+VALUES
+('Lucas Henrique', 'henrique.adelino@gmail.com', 'lucas123',FALSE, 0.0, "UsuarioPaciente", 0.0),
+('Victor Aquino', 'victor.aquino@gmail.com', 'victor1020',FALSE, 0.0, "UsuarioPaciente", 0.0),
+('Victor Ferreira', 'victor.ferreira@gmail.com', 'ferreiraV10',FALSE, 0.0, "UsuarioPaciente", 0.0),
+('Pedro Altoe', 'pedro.atoe@gmail.com', 'pedrao2030',FALSE, 0.0, "UsuarioPaciente", 0.0),
+('Kevin Dias', 'kevin.dias@gmail.com', 'kdias100',FALSE, 0.0, "UsuarioPaciente", 0.0);
+
+INSERT INTO tb_usuarios
+(nome_usuario, email_usuario, senha_usuario, autenticado, experiencia, tipo_conta, avaliacao)
+VALUES
+    ('Taciane Pinheiro', 'taci@gmail.com', 'taci1020',FALSE, 0.0, "UsuarioNutri", 0.0),
+    ('Renato Cariane', 'rcariane@gmail.com', 'renato123',FALSE, 0.0, "UsuarioNutri", 0.0);
